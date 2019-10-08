2b45969fee55b042b46b1c7c909631be847e98b3
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-08 16:58:07 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
 // your Bomb code here!
+import React, {Component} from 'react';
+
+ class Bomb extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      secondsLeft: props.initialCount,
+    }
+  }
+
+
+  render(){
+    return(
+      <div className="bomb">
+        {(this.state.secondsLeft > 0) ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!' }
+
+      </div>
+    );
+  }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,23 @@
 // your ImageSlider code here!
+import React, {Component} from 'react';
+
+class ImageSlider extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+
+  render(){
+    return(
+        <div className="slider">
+          I am on slide {this.state.currentSlideIndex}
+        </div>
+    );
+  }
+}
+
+export default ImageSlider;
