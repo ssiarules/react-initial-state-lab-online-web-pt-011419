078d5362c2824c8176ff05ceab990e73a7130976
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-08 16:39:23 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -3,10 +3,20 @@ import React, {component} from 'react';
 
 export default class ImageSlider extends Component{
 
+  constructor(){
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+
+  render(){
+    return(
+
+    )
+  }
+
 }
 
-render(){
-  return(
 
-  )
-}
